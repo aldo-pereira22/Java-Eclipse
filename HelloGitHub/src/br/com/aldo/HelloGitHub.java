f249812a0048b4fb3b880e66d9a4e110ac2e5192
aldo <aldo@aldo-VirtualBox>
2020-10-16 14:35:54 -0300

    teste

--- HelloGitHub/src/br/com/aldo/HelloGitHub.java
+++ HelloGitHub/src/br/com/aldo/HelloGitHub.java
@@ -5,6 +5,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		//Exemplo de integração do Eclipse com o GitHub
 		System.out.println("Hello Git, um exemplo do git com o Eclipse");
+		System.out.println("Adcionei mais uma linha de código para teste");
 		
 	}
 
