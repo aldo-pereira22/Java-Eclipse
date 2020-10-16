fce01501bb83395c182349309c5132050155b468
aldo-pereira22 <35982594+aldo-pereira22@users.noreply.github.com>
2020-10-16 12:33:33 -0300

    Adicionei arquivos via upload

---
+++ HelloGitHub/src/br/com/aldo/HellGit.java
@@ -0,0 +1,11 @@
+package br.com.aldo;
+
+public class HellGit {
+
+	public static void main(String[] args) {
+		//Exemplo de integração do Eclipse com o GitHub
+		System.out.println("Hello Git, um exemplo do git com o Eclipse");
+		
+	}
+
+}
